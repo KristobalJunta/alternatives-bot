15a0dee29b93ab458159ee94f0f2fb0fbaeff2e8
Kristobal <junta.kristobal@gmail.com>
2017-07-27 14:30:05 +0300

    first commit; created basic bot, plus readme and requirements

---
+++ README.md
@@ -0,0 +1,11 @@
+alternatives-bot
+
+# Alternatives Bot (aka Escobar)
+
+This is a hello-world-like telegram bot based on an old Russian meme.
+
+## Перед вами группа Bredor.
+
+![Шо?](escobar.jpg)
+
+Я Эскобар.
---
+++ main.py
@@ -0,0 +1,42 @@
+# _*_ coding: utf-8 _*_
+
+import telebot
+import config
+
+
+bot = telebot.TeleBot(config.token)
+
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    print('received help command')
+    response = "Я Эскобар."
+    bot.send_message(message.chat.id, response)
+
+
+@bot.message_handler(commands=['help'])
+def handle_help(message):
+    print('received help command')
+    response = "— Почему группа называется именно «Бредор»?\n— А ну хер его знает, ну Бредор, мля, ну Бредор..."
+    bot.send_message(message.chat.id, response)
+
+
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    print('received a message')
+    if "или" in message.text:
+        response = "Шо то хуйня, шо это хуйня. Вот это обе хуйни такие, шо я, бля, ебал её маму у рот."
+        bot.send_message(message.chat.id, response)
+    else:
+        bot.send_message(message.chat.id, 'Шо?')
+
+
+@bot.inline_handler(lambda query: query.query == 'text')
+def query_text(inline_query):
+    try:
+        bot.answer_inline_query(inline_query.id, "йоу")
+    except Exception as e:
+        print(e)
+
+
+bot.polling(none_stop=True, interval=0)
